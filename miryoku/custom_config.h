e269055f09dbdf5474c33660c50aa3c73685c199
AidanM <74642918+AidanDMarkham@users.noreply.github.com>
2025-03-11 08:07:51 -0500

    Update custom_config.h
    U_MED to U_MEDIA

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -5,7 +5,7 @@
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
 &kp A,             &kp R,             &kp S,             &kp T,             &kp G,             &kp M,             &kp N,             &kp E,             &kp I,             &kp O,             \
 &kp Z,             &kp X,             &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
-U_NP,              U_NP,              &mo U_MED,         &mo U_NAV,         &kp SPACE,         &kp LSHFT,         &mo U_NUM,         &mo U_SYM,         U_NP,              U_NP
+U_NP,              U_NP,              &mo U_MEDIA,       &mo U_NAV,         &kp SPACE,         &kp LSHFT,         &mo U_NUM,         &mo U_SYM,         U_NP,              U_NP
 
 #define MIRYOKU_LAYER_NAV \
 &kp ESC,           U_NA,              U_NA,              U_NA,              LA(LS(TAB)),       U_UND,             U_PST,             U_CPY,             U_CUT,             U_RDO,             \
